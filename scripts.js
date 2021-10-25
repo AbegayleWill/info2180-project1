6db69da3d8d8b597275fa212614336693e0d6f3d
Abegayle Williams <abewilli10@gmail.com>
2021-10-24 19:10:01 -0500

    Edited Javascript to allow form to be submitted and message displayed, edited message class in css file.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 /* Add your JavaScript to this file */
+window.onload = function(){windowLoad()}
+       
+    function windowLoad(){
+   
+    const form = document.querySelector('form')
+
+        form.addEventListener('submit',function(event){
+            
+            event.preventDefault()//prevents the form from auto submitting
+
+            let emailAddress = document.getElementById('email').value
+            let message = document.querySelector('.message')
+            if (emailAddress=="")   {
+                //alert("Invalid")
+                message.innerHTML= "Please enter valid email Address"
+                return false
+            }else{
+                message.innerHTML= "Thankyou! Your email address " + emailAddress + " has been added to our mailing list!"
+                form.reset()
+                //alert('submitted')
+                return false
+                
+            }
+            
+                
+        })
+    }
